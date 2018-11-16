6163f71baf008b14af27afc285beb892e50829ae
Murilo Erhardt <murilo0121@gmail.com>
2018-11-16 01:43:48 -0200

    adicionando disciplina

---
+++ Universistema/Universistema/View/DisciplinaPage.aspx
@@ -0,0 +1,70 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true"  MasterPageFile="~/Site.Master" CodeFile="DisciplinaPage.aspx.cs" Inherits="View_Disciplina" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <div>
+        <h1><asp:Label Text="Administração de Disciplinas" ID="statusLabel" runat="server"/></h1>
+        <p>
+            Selecione Um Curso:  
+            <asp:DropDownList 
+                id="cursoList" 
+                DataTextField="nome" 
+                DataValueField="idcursos"  
+                runat="server"  
+                onselectedindexchanged="cursoList_SelectedIndexChanged"
+                AutoPostBack="true">
+            </asp:DropDownList>
+
+            <asp:DropDownList 
+                id="periodosList" 
+                DataTextField="numPeriodo" 
+                DataValueField="idperiodos"  
+                runat="server"  
+                onselectedindexchanged="periodosList_SelectedIndexChanged"
+                AutoPostBack="true">
+            </asp:DropDownList>
+        </p>
+        <p>
+            <asp:Panel id="PainelCadastro" runat="server">
+                    Nome Disciplina:<br />
+                    <asp:TextBox runat="server" ID="nomeDisciplina" CssClass="form-group"/><br />
+                    AT:<br />
+                    <asp:TextBox runat="server" ID="at" CssClass="form-group"/><br />
+                    AP:<br />
+                    <asp:TextBox runat="server" ID="ap" CssClass="form-group"/><br />
+                    Creditos:<br />
+                    <asp:TextBox runat="server" ID="cred" CssClass="form-group"/><br />
+                    Horas Relógio:<br />
+                    <asp:TextBox runat="server" ID="hr" CssClass="form-group"/><br />
+                    Horas Aula:<br />
+                    <asp:TextBox runat="server" ID="ha" CssClass="form-group"/><br /> 
+                    
+                    <asp:Button runat="server" ID="btnCadastrar" Text="Adicionar" OnClick="btnCadastrar_Click" />
+                    <asp:Button runat="server" ID="btnEditar" Text="Editar"  OnClick="btnEditar_Click" />
+            </asp:Panel>
+        </p>
+    </div>
+
+     <div>
+        <h1>Lista de Disciplinas</h1>
+        <asp:GridView id="disciplinasList" runat="server" AutoGenerateColumns="false" CssClass="table table-striped">
+            <Columns>
+                <asp:BoundField DataField="nome" HeaderText="Nome" />
+                <asp:BoundField DataField="at" HeaderText="AT" />
+                <asp:BoundField DataField="ap" HeaderText="AP" />
+                <asp:BoundField DataField="cred" HeaderText="CRED" />
+                <asp:BoundField DataField="hr" HeaderText="HR" />
+                <asp:BoundField DataField="ha" HeaderText="HA" />
+                <asp:TemplateField>
+                    <ItemTemplate>
+                        <asp:LinkButton  ID="editSelect" text="Editar" runat="server"  CommandArgument='<%# Eval("idDisciplinas") %>' OnClick="editSelect_Click" />
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField>
+                    <ItemTemplate>
+                        <asp:LinkButton  ID="apagarSelect" text="Apagar" runat="server"  CommandArgument='<%# Eval("idDisciplinas") %>' OnClick="apagarSelect_Click" />
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+        </asp:GridView>
+    </div>
+</asp:Content>
---
+++ Universistema/Universistema/View/DisciplinaPage.aspx.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+
+public partial class View_Disciplina : System.Web.UI.Page
+{
+    static string connString = "Server=localhost;Database=universistema;Uid=root;Pwd=admin";
+    MySqlConnection connection = new MySqlConnection(connString);
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        btnEditar.Visible = false;
+        PainelCadastro.Visible = false;
+
+        if (!IsPostBack)
+        {
+
+            loadCursos();
+            cursoList.SelectedIndex = 0;
+            loadPeriodos(Convert.ToInt32(cursoList.SelectedItem.Value));
+            periodosList.SelectedIndex = 0;
+            loadDisciplinas(Convert.ToInt32(periodosList.SelectedItem.Value));
+
+
+        }
+        else
+        {
+            var indiceCurso = cursoList.SelectedIndex;
+            var indicePeriodo = periodosList.SelectedIndex;
+            if(indiceCurso != -1 && indicePeriodo != -1)
+            {
+                loadCursos();
+                PainelCadastro.Visible = true;
+                periodosList.Visible = true;
+                cursoList.SelectedIndex = indiceCurso;
+                periodosList.SelectedIndex = indicePeriodo;
+                try
+                {
+                    loadDisciplinas(Convert.ToInt32(periodosList.SelectedItem.Value));
+                }
+                catch
+                {
+                }
+                
+            }
+
+        }
+    }
+
+    private void loadCursos()
+    {
+        periodosList.Visible = false;
+        using (MySqlConnection sqlCon = new MySqlConnection())
+        {
+            connection.Open();
+            MySqlDataReader reader = null;
+
+            MySqlCommand command = new MySqlCommand("SELECT * FROM cursos", connection);
+
+            cursoList.DataSource = command.ExecuteReader();
+            cursoList.DataBind();
+            connection.Close();
+        }
+    }
+
+    private void loadPeriodos(int idCurso)
+    {
+        periodosList.Visible = false;
+        using (MySqlConnection sqlCon = new MySqlConnection())
+        {
+            connection.Open();
+            try
+            {
+                MySqlCommand command = new MySqlCommand("SELECT * FROM periodos WHERE cursoId='" + idCurso + "'", connection);
+                periodosList.Visible = true;
+                periodosList.DataSource = command.ExecuteReader();
+                periodosList.DataBind();
+                connection.Close();
+            }
+            catch
+            {
+                Response.Write("Não Encontrado");
+                cursoList.SelectedIndex = 0;
+                periodosList.SelectedIndex = 0;
+
+            }
+
+        }
+    }
+
+    private void loadDisciplinas(int idPeriodo)
+    {
+        using (MySqlConnection sqlCon = new MySqlConnection())
+        {
+            connection.Open();
+
+            MySqlCommand command = new MySqlCommand("SELECT * FROM disciplinas WHERE periodoId='" + idPeriodo + "'", connection);
+            disciplinasList.DataSource = command.ExecuteReader();
+            disciplinasList.DataBind();
+            connection.Close();
+        }
+    }
+
+    protected void cursoList_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        periodosList.Visible = true;
+        int idDoCurso = Convert.ToInt32(cursoList.SelectedItem.Value);
+        loadPeriodos(idDoCurso);
+    }
+
+    protected void periodosList_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        int idPeriodo = Convert.ToInt32(periodosList.SelectedItem.Value);
+        loadDisciplinas(idPeriodo);
+        PainelCadastro.Visible = true;
+    }
+
+    protected void btnCadastrar_Click(object sender, EventArgs e)
+    {
+        int idDoCurso = Convert.ToInt32(cursoList.SelectedItem.Value);
+        int idDoPeriodo = Convert.ToInt32(periodosList.SelectedItem.Value);
+
+        using (MySqlConnection sqlCon = new MySqlConnection())
+        {
+            connection.Open();
+            MySqlCommand comm = connection.CreateCommand();
+            comm.CommandText = "INSERT INTO `universistema`.`disciplinas` (`nome`, `at`, `ap`, `cred`, `hr`, `ha`, `periodoId`) " +
+                                "VALUES(@nome, @at, @ap, @cred, @ht, @ha, @idDoPeriodo)";
+
+            comm.Parameters.AddWithValue("@nome", nomeDisciplina.Text);
+            comm.Parameters.AddWithValue("@at", at.Text);
+            comm.Parameters.AddWithValue("@ap", ap.Text);
+            comm.Parameters.AddWithValue("@cred", cred.Text);
+            comm.Parameters.AddWithValue("@ht", hr.Text);
+            comm.Parameters.AddWithValue("@ha", ha.Text);
+            comm.Parameters.AddWithValue("@idDoPeriodo", idDoPeriodo);
+            comm.ExecuteNonQuery();
+            connection.Close();
+            //numeroPeriodo.Text = "";
+
+
+        }
+        loadDisciplinas(Convert.ToInt32(periodosList.SelectedItem.Value));
+        clearFields();
+
+    }
+
+    private void clearFields()
+    {
+        nomeDisciplina.Text = null;
+        at.Text = null;
+        ap.Text = null;
+        cred.Text = null;
+        hr.Text = null;
+        ha.Text = null;
+    }
+
+    protected void btnEditar_Click(object sender, EventArgs e)
+    {
+        var argument = ((Button)sender).CommandArgument;
+        string idCurso = argument.ToString();
+        using (MySqlConnection sqlCon = new MySqlConnection())
+        {
+            MySqlDataReader reader = null;
+            connection.Open();
+            MySqlCommand command = new MySqlCommand("UPDATE `universistema`.`disciplinas` SET `nome` = '" + nomeDisciplina.Text + 
+                                                    "', `at` = '" + at.Text +
+                                                    "', `ap` = '" + ap.Text +
+                                                    "', `cred` = '" + cred.Text +
+                                                    "', `ha` = '" + ha.Text +
+                                                    "', `hr` = '" + hr.Text +
+                                                    "' WHERE(`iddisciplinas` = '" + idCurso + "')", connection);
+            DataTable dtbl = new DataTable();
+            command.ExecuteNonQuery();
+            connection.Close();
+            clearFields();
+            loadDisciplinas(Convert.ToInt32(periodosList.SelectedItem.Value));
+            btnCadastrar.Visible = true;
+            btnEditar.Visible = false;
+            statusLabel.Text = "Novo Periodo";
+
+        }
+    }
+
+    protected void editSelect_Click(object sender, EventArgs e)
+    {
+        int idCurso = Convert.ToInt32((sender as LinkButton).CommandArgument);
+        btnCadastrar.Visible = false;
+        btnEditar.Visible = true;
+        btnEditar.CommandArgument = idCurso.ToString();
+
+        using (MySqlConnection sqlCon = new MySqlConnection())
+        {
+            MySqlDataReader reader = null;
+            connection.Open();
+            MySqlCommand command = new MySqlCommand("SELECT * FROM disciplinas WHERE iddisciplinas='" + idCurso + "'", connection);
+            DataTable dtbl = new DataTable();
+            reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                statusLabel.Text = "Editar periodo";
+                var temp = reader["nome"];
+                string stringText = Convert.ToString(temp);
+                nomeDisciplina.Text = stringText;
+
+                temp = reader["at"];
+                stringText = Convert.ToString(temp);
+                at.Text = stringText;
+
+                temp = reader["ap"];
+                stringText = Convert.ToString(temp);
+                ap.Text = stringText;
+
+                temp = reader["cred"];
+                stringText = Convert.ToString(temp);
+                cred.Text = stringText;
+
+                temp = reader["hr"];
+                stringText = Convert.ToString(temp);
+                hr.Text = stringText;
+
+                temp = reader["ha"];
+                stringText = Convert.ToString(temp);
+                ha.Text = stringText;
+
+            }
+            connection.Close();
+        }
+    }
+
+    protected void apagarSelect_Click(object sender, EventArgs e)
+    {
+        int idDisciplina = Convert.ToInt32((sender as LinkButton).CommandArgument);
+        using (MySqlConnection sqlCon = new MySqlConnection())
+        {
+            connection.Open();
+            MySqlCommand comm = connection.CreateCommand();
+            comm.CommandText = " DELETE FROM `universistema`.`disciplinas` WHERE(`iddisciplinas` = '" + idDisciplina + "');";
+
+            comm.ExecuteNonQuery();
+            connection.Close();
+            Response.Write("DISCIPLINA DELETADA");
+            loadDisciplinas(Convert.ToInt32(periodosList.SelectedItem.Value));
+        }
+    }
+}
